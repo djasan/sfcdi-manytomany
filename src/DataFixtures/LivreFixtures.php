c57cce4e3fc790d0b499acaef1af931bd3352a1d
sanson <sansondjamila@orange.fr>
2024-02-05 09:25:52 +0100

    mm

--- src/DataFixtures/LivreFixtures.php
+++ src/DataFixtures/LivreFixtures.php
@@ -11,14 +11,25 @@ class LivreFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-
         $faker = Factory::create('fr_FR');
-        
-        for($j=0 ; $j<= 5 ; $j++):
+
+        // Créer quelques livres
+        $livre1 = new Livre();
+        $livre1->setAuteur($faker->lastName);
+        $manager->persist($livre1);
+
+        $livre2 = new Livre();
+        $livre2->setAuteur($faker->lastName);
+        $manager->persist($livre2);
+
+        $manager->flush();
+
+        for ($j = 0; $j <= 5; $j++):
             $livre = new Livre();
 
-            $livre->setTitre($faker->words())
-            ->setEditeur($faker->sentence());
+            $livre->setTitre($faker->sentence())
+                ->setEditeur($faker->sentence())
+                ->setAuteur($faker->randomElement([$livre1, $livre2]));
 
             $manager->persist($livre);
         endfor;
---
+++ src/Entity/Auteur.php
@@ -0,0 +1,75 @@
+<?php
+
+// src/Entity/Auteur.php
+
+namespace App\Entity;
+
+use App\Repository\AuteurRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AuteurRepository::class)]
+class Auteur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\OneToMany(mappedBy: 'auteur', targetEntity: Livre::class)]
+    private Collection $livres;
+
+    public function __construct()
+    {
+        $this->livres = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Livre>
+     */
+    public function getLivres(): Collection
+    {
+        return $this->livres;
+    }
+
+    public function addLivre(Livre $livre): static
+    {
+        if (!$this->livres->contains($livre)) {
+            $this->livres->add($livre);
+            $livre->setAuteur($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLivre(Livre $livre): static
+    {
+        if ($this->livres->removeElement($livre)) {
+            // Assurez-vous de désassocier le livre de l'auteur
+            $livre->setAuteur(null);
+        }
+
+        return $this;
+    }
+}
